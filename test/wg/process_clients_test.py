282561f2342563e46d5391e38b5ffa5e52886b74
spector <spector@borun.ru>
2024-03-07 13:58:19 +0300

    separate tests

--- test/wg/process_clients_test.py
+++ test/wg/process_clients_test.py
@@ -35,29 +35,6 @@ class ProcessClientsTest(unittest.TestCase):
     def tearDown(self) -> None:
         shutil.rmtree(self.temp_dir)
 
-    def test_get_existing_clients__clients_exists(self) -> None:
-        config = {'clients': self.clients}
-        expected_existing_clients = self.clients
-        self.assertEqual(expected_existing_clients,
-                         common.get_existing_clients(config))
-
-    def test_get_existing_clients__no_clients_exists(self) -> None:
-        config = {}
-        self.assertEqual([], common.get_existing_clients(config))
-
-    def test_get_new_clients_names__clients_exists(self) -> None:
-        existing_clients = self.clients
-        passed_client_names = ['client1', 'client4']
-        self.assertEqual(['client4'],
-                         common.get_new_clients_names(passed_client_names, existing_clients))
-
-    def test_get_new_clients_names__no_clients_exists(self) -> None:
-        existing_clients = []
-        passed_client_names = ['client1', 'client2']
-        self.assertEqual(passed_client_names.sort(),
-                         common.get_new_clients_names(passed_client_names,
-                                                      existing_clients).sort())
-
     def test_passed_client_names__clients_exists(self) -> None:
         client_name = 'new_client'
         existing_clients = self.clients
@@ -85,21 +62,6 @@ class ProcessClientsTest(unittest.TestCase):
                          wireguard.generate_new_client(client_name,
                                                        existing_clients,
                                                        '10.9.0.0/24'))
-
-    def test_get_clients_after_removing__no_clients_to_remove(self) -> None:
-        existing_clients = self.clients
-        clients_names_to_remove = []
-        actulal_clients = common.get_clients_after_removing(existing_clients,
-                                                            clients_names_to_remove)
-        self.assertEqual(existing_clients, actulal_clients)
-
-    def test_get_clients_after_removing__clients_to_remove_exists(self) -> None:
-        existing_clients = self.clients
-        clients_names_to_remove = [self.clients[1]['name'], self.clients[0]['name']]
-        expected_clients_list = [self.clients[2]]
-        actual_clients_list = common.get_clients_after_removing(existing_clients,
-                                                                clients_names_to_remove)
-        self.assertEqual(expected_clients_list, actual_clients_list)
 
     def test_remove_clients_configs__clients_dir_not_exists(self) -> None:
         shutil.rmtree(self.clients_dir)
