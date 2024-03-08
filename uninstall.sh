f49941ef793ae1a06d48d826cdcf487993cc94d1
spector <spector@borun.ru>
2024-03-08 21:45:20 +0300

    soon :)

--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-rm -rf /usr/local/lib/veepeenet \
-  /etc/veepeenet/wg \
-  /etc/veepeenet/xray \
-  /var/log/veepeenet/wg \
-  /var/log/veepeenet/xray \
-  /usr/local/lib/veepeenet/wireguard.py \
-  /usr/local/lib/veepeenet/xray.py \
-  /usr/local/bin/wg-config \
-  /usr/local/bin/xray-config
