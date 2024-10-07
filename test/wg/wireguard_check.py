89079a636ddb20ac22ceb11315c83f8919fd7185
spector <spector@borun.ru>
2024-10-07 13:40:46 +0300

    fix check runs

--- test/wg/wireguard_check.py
+++ test/wg/wireguard_check.py
@@ -8,6 +8,7 @@ common.ROUTE_FILE_PATH = 'res/proc/net/route'
 common.UFW_BEFORE_RULES_PATH = 'res/etc/ufw/before.rules.modified'
 common.SYSCTL_FILE_PATH = 'res/etc/sysctl.conf'
 common.FORWARD_POLICY_FILE = 'res/etc/default/ufw'
+common.META_FILE_PATH = 'res/meta.json'
 
 common.CONFIG_PATH = 'output/config.json'
 common.DEFAULT_CLIENTS_DIR = 'output/clients'
--- test/xtls/xray_check.py
+++ test/xtls/xray_check.py
@@ -7,6 +7,7 @@ xray.XRAY_CONF_PATH = 'res/etc/xray'
 
 common.CONFIG_PATH = 'output/config.json'
 common.RESULT_LOG_PATH = 'output/result.json'
+common.META_FILE_PATH = 'res/meta.json'
 sys.argv.append('--check')
 
 xray.main()
