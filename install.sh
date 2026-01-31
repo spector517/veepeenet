58f0658b642cf61c368e4cc65967fc9aa7d6ba9f
spector517 <spector@borun.ru>
2026-01-31 12:35:11 +0300

    installation fixes

--- install.sh
+++ install.sh
@@ -8,12 +8,18 @@ if [[ "$EUID" -ne 0 ]]; then
 fi
 
 rm -rf /usr/local/lib/veepeenet
-rm -rf /usr/local/bin/xrayctl
+rm -f /usr/local/bin/xrayctl
+rm -f /usr/local/bin/xray-migrate-1-to-2
 mkdir -p /usr/local/lib/veepeenet
 
+apt update
+apt install -y python3.12-venv
+
 python3 -m venv /usr/local/lib/veepeenet/venv
+/usr/local/lib/veepeenet/venv/bin/pip3 install --upgrade pip
 /usr/local/lib/veepeenet/venv/bin/pip3 install ./veepeenet*
 
 ln -s /usr/local/lib/veepeenet/venv/bin/xrayctl /usr/local/bin/xrayctl
+ln -s /usr/local/lib/veepeenet/venv/bin/xray-migrate-1-to-2 /usr/local/bin/xray-migrate-1-to-2
 
 echo 'VeePeeNET successfully installed'
